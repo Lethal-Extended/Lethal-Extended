a7fef5196e71ed8c28ba7ac62c5de7a7d6a295b0
Pengu <pengu@galactiq.net>
2023-12-10 06:47:42 -0600

    0.1.0

--- update-other.bat
+++
@@ -1,5 +0,0 @@
-set /p UserInputPath=What Drive is the game installed to (Letter only)
-
-DEL "%UserInputPath%:\SteamLibrary\steamapps\common\Lethal Company\BepInEx\plugins\EmployeeAssignments.dll"
-DEL "%UserInputPath%:\SteamLibrary\steamapps\common\Lethal Company\BepInEx\plugins\LC_API.dll"
-DEL "%UserInputPath%:\SteamLibrary\steamapps\common\Lethal Company\BepInEx\plugins\LethalPing.dll"
--- update.bat
+++
@@ -1,4 +0,0 @@
-DEL "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx\plugins\EmployeeAssignments.dll"
-DEL "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx\plugins\LC_API.dll"
-DEL "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx\plugins\LethalPing.dll"
-pause
