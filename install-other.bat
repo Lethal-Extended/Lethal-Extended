ae9124de4ee5baa977a800f3797ecfbe1aec7b39
Pengu <pengu@galactiq.net>
2023-12-18 10:42:07 -0600

    0.1.3

--- install-other.bat
+++ install-other.bat
@@ -1,4 +1,7 @@
 @echo off
 set /p UserInputPath=What Drive is the game installed to (Letter only)
 
+del /q /s "%UserInputPath%:\SteamLibrary\steamapps\common\Lethal Company\BepInEx\*"
+
 xcopy /e .\ "%UserInputPath%:\SteamLibrary\steamapps\common\Lethal Company" /Y
+pause
--- install.bat
+++ install.bat
@@ -1,1 +1,2 @@
+del /q /s "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx"
 xcopy /e .\ "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company" /Y
